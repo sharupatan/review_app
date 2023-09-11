e84511623739ecb3b27e2db999db04b652933d3c
apple <apple@apples-MacBook-Pro.local>
2023-09-12 05:28:26 +0530

    authorization full fedged app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::API
   include UsersHelper
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
   
   rescue_from CanCan::AccessDenied do |exception|
     render json: 'You are not authorized!'
   end
-
+  
   def profile
 		render json: current_user
 	end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name role])
+    # devise_parameter_sanitizer.permit(:account_update, keys: %i[name role])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_11_200824) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_11_233325) do
+  create_table "reviews", force: :cascade do |t|
+    t.text "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "role", default: "", null: false
@@ -27,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_11_200824) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reviews", "users"
 end
